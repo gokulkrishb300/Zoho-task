02e3ec769dee79b4d3fb702276ac2100ee14a4c5
gokulkrishb300 <95754748+gokulkrishb300@users.noreply.github.com>
2022-03-22 21:04:02 +0530

    Add files via upload

---
+++ PhaseTwoCal.js
@@ -0,0 +1,178 @@
+var value1 = 0;
+var value2 = 0;
+var result = 0;
+var arithmetic ='';
+var temp = 0;
+
+function input(values){
+document.getElementById("display").value += values;
+
+if(value1!=0 && (arithmetic =='+' || arithmetic =='-' || arithmetic =='*' || arithmetic == '/' || arithmetic =='+-' || arithmetic =='--' || arithmetic =='*-' || arithmetic == '/-')) {
+value2 = document.getElementById("display").value;
+console.log("value2 : " +value2);
+}
+
+
+else{
+value1 = document.getElementById("display").value;
+console.log("value1 : " + value1);
+}
+
+
+}
+
+
+function subdisplay(values){
+document.getElementById("subdisplay").value += values;
+}
+
+function symbol(sign){
+ arithmetic += sign;
+ 
+ if(value1==0  && (sign == '+' || sign == '-')){
+ document.getElementById("display").value = sign;
+ }
+ 
+ else if(value1 ==0 && (sign == '*' || sign == '/')){
+ document.getElementById("display").value = '';
+ }
+ 
+ else if(value1!=0 && arithmetic.length==1){
+ arithmetic = arithmetic;
+ glow();
+ console.log("arithmetic: " +arithmetic);
+ }
+ 
+ else if(value1!=0 && arithmetic.length==2 && (arithmetic =='+-' || arithmetic =='--' ||   arithmetic =='*-' || arithmetic == '/-')){
+ arithmetic = arithmetic.substring(0);
+ document.getElementById("display").value = '-';
+ glow();
+ console.log("arithmetic: " +arithmetic);
+ }
+ 
+  else if(value2!=0 && (sign =='+' || sign=='-' || sign=='*' || sign=='/')){
+ 
+ 
+  
+ if(sign=='+'){
+  document.getElementById("display").value = ' ';
+ document.getElementById("display").value = Number(value1) + Number(value2);
+ 
+ temp =  document.getElementById("display").value;
+ 
+
+ 
+ console.log("temp :" + temp);
+ }
+ else if(sign =='-'){
+
+ }
+ }
+ else if(value1!=0 && arithmetic.length>1){
+ arithmetic = sign;
+ glow();
+ console.log("arithmetic: " +arithmetic);
+ }
+ 
+
+ 
+}
+
+
+
+function glow(){
+if(arithmetic == '+'){
+document.getElementById("display").value = ' ';
+document.getElementById("add").style.background = "#ff8000";
+document.getElementById("sub").style.background = "white";
+document.getElementById("mul").style.background = "white";
+document.getElementById("div").style.background = "white";
+}
+
+else if(arithmetic == '-'){
+document.getElementById("display").value = ' ';
+document.getElementById("sub").style.background = "#ff8000";
+document.getElementById("add").style.background = "white";
+document.getElementById("mul").style.background = "white";
+document.getElementById("div").style.background = "white";
+}
+
+else if(arithmetic == '*'){
+document.getElementById("display").value = ' ';
+document.getElementById("mul").style.background = "#ff8000";
+document.getElementById("add").style.background = "white";
+document.getElementById("sub").style.background = "white";
+document.getElementById("div").style.background = "white";
+}
+else if(arithmetic == '/'){
+document.getElementById("display").value = ' ';
+document.getElementById("div").style.background = "#ff8000";
+document.getElementById("add").style.background = "white";
+document.getElementById("sub").style.background = "white";
+document.getElementById("mul").style.background = "white";
+}
+}
+
+function evaluater() {
+
+if(arithmetic == '+'){
+result = Number(value1) + Number(value2);
+
+}
+
+else if(arithmetic == '-'){
+result = value1 - value2;
+
+}
+
+else if(arithmetic == '*'){
+result = value1 * value2;
+
+}
+
+else if(arithmetic == '/'){
+result = value1 / value2;
+}
+
+else if(arithmetic == '+-'){
+result = Number(value1) + -Number(value2.substring(1));
+console.log(result);
+}
+
+else if(arithmetic == '--'){
+result = Number(value1) + Number(value2.substring(1));
+console.log(result);
+}
+
+else if(arithmetic == '*-'){
+result = Number(value1) * -Number(value2.substring(1));
+console.log(result);
+}
+
+else if(arithmetic == '/-'){
+result = Number(value1) / -Number(value2.substring(1));
+console.log(result);
+}
+
+
+
+document.getElementById("display").value = ' ';
+document.getElementById("display").value = result;
+document.getElementById("add").style.background = "white";
+document.getElementById("sub").style.background = "white";
+document.getElementById("mul").style.background = "white";
+document.getElementById("div").style.background = "white";
+
+}
+
+function reloadC(){
+evaluater();
+document.getElementById("display").value = '';
+value1 = 0;
+console.log("value1 : "+value1);
+value2 = 0;
+console.log("value2 : "+value2);
+arithmetic ='';
+console.log("arithmetic : "+arithmetic);
+
+}
